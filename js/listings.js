93c096be694f1151aabdcc7b3a256147f3ccbf77
Josh Soileau <joshua.soileau@gmail.com>
2017-01-17 07:23:03 -0500

    More listing changes

--- js/listings.js
+++ js/listings.js
@@ -1,48 +1,76 @@
 var listings = [
     {
-        address: "123 Any Street",
-        type: "Homesite",
+        address: "529 Country Place Road",
+        type: "Single Family Home",
         bedrooms: 4,
         bathrooms: 2,
         price: "$191,500",
         sqft: "3,200 sqft",
-        defaultImage: "images/listings/default.jpg",
+        defaultImage: "images/listings/529-country-place-road.jpg",
         images: [
-            "images/listings/default.jpg",
-            "images/listings/default.jpg",
-            "images/listings/default.jpg"
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
         ]
     },
     {
-        address: "123 Any Street",
-        type: "Homesite",
+        address: "742 Hibbens Grant Boulevard",
+        type: "Single Family Home",
         bedrooms: 4,
         bathrooms: 2,
         price: "$191,500",
         sqft: "3,200 sqft",
-        defaultImage: "images/listings/default.jpg",
+        defaultImage: "images/listings/742-hibbens-grant-blvd.jpg",
         images: [
-            "images/listings/default.jpg",
-            "images/listings/default.jpg",
-            "images/listings/default.jpg"
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
         ]
     },
     {
-        address: "123 Any Street",
+        address: "521 Country Place Road",
         type: "Homesite",
         bedrooms: 4,
         bathrooms: 2,
         price: "$191,500",
         sqft: "3,200 sqft",
-        defaultImage: "images/listings/default.jpg",
+        defaultImage: "images/listings/521-country-place-road.jpg",
         images: [
-            "images/listings/default.jpg",
-            "images/listings/default.jpg",
-            "images/listings/default.jpg"
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
         ]
     },
     {
-        address: "123 Any Street",
+        address: "881 Tupelo Bay Drive ",
+        type: "Single Family Home",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/881-tupelo-bay-drive.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "716 Stucco Lane",
+        type: "Single Family Home",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/716-stucco-lane.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "341 Bridgetown Pass",
         type: "Homesite",
         bedrooms: 4,
         bathrooms: 2,
@@ -50,9 +78,93 @@ var listings = [
         sqft: "3,200 sqft",
         defaultImage: "images/listings/default.jpg",
         images: [
-            "images/listings/default.jpg",
-            "images/listings/default.jpg",
-            "images/listings/default.jpg"
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "353 Bridgetown Pass",
+        type: "Homesite",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/default.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "363 Bridgetown Pass",
+        type: "Homesite",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/default.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "355 Bridgetown Pass",
+        type: "Homesite",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/default.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "359 Bridgetown Pass",
+        type: "Homesite",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/default.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "372 Bridgetown Pass",
+        type: "Homesite",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/default.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
+        ]
+    },
+    {
+        address: "376 Bridgetown Pass",
+        type: "Homesite",
+        bedrooms: 4,
+        bathrooms: 2,
+        price: "$191,500",
+        sqft: "3,200 sqft",
+        defaultImage: "images/listings/default.jpg",
+        images: [
+            "images/listings/742-hibbens-grant-blvd.jpg",
+            "images/listings/716-stucco-lane.jpg",
+            "images/listings/521-country-place-road.jpg"
         ]
     }
 
@@ -84,17 +196,15 @@ jQuery(document).ready(function($) {
                                 '<p class="price"><strong class="title">Price:</strong>#{price}</p>' +
                                 '<p class="type"><strong class="title">Type:</strong>#{type}</p>' +
                                 '<p class="bedrooms"><strong class="title">Bedrooms:</strong>#{bedrooms}</p>' +
-                                '<p class="sqft"><strong class="title">Sqft:</strong>#{sqft}</p>' +
                                 '<p class="bathrooms"><strong class="title">Bathrooms:</strong>#{bathrooms}</p>' +
+                                '<p class="sqft"><strong class="title">Sqft:</strong>#{sqft}</p>' +
                                 '<p class="contact">For more information <a href="mailto:info@hibbenatbellehall.com?subject=Information%20Request%20for%20'+swapSpaces(listing.address)+'">click here</a> to email an agent today.</p>' +
                             '</div>' +
                         '</div>' +
                         '<div class="row no-gutters">' +
-                            '<div class="col-sm-6">' +
-                                '<img src="#{images[1]}" alt="">' +
+                            '<div class="col-sm-6 picture" style="background-image: url(#{images[1]})">' +
                             '</div>' +
-                            '<div class="col-sm-6">' +
-                                '<img src="#{images[2]}" alt="">' +
+                            '<div class="col-sm-6 picture" style="background-image: url(#{images[2]})">' +
                             '</div>' +
                         '</div>' +
                     '</div>' +
