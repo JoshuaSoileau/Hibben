e2e4c9b7639741b3da69da1971e50c0bff4ab71e
Josh Soileau <joshua.soileau@gmail.com>
2017-01-21 10:08:31 -0500

    Initial logic for videos

---
+++ js/video.js
@@ -0,0 +1,62 @@
+var videos = [
+    {
+        thumbnail: "images/gallery/thumbnails/356/8.jpg",
+        mp4: "videos/example.mp4",
+        ogg: "videos/example.ogg",
+        webm: "videos/example.webm"
+    },
+    {
+        thumbnail: "images/gallery/thumbnails/351/10.jpg",
+        mp4: "videos/example.mp4",
+        ogg: "videos/example.ogg",
+        webm: "videos/example.webm"
+    },
+    {
+        thumbnail: "images/gallery/thumbnails/356/9.jpg",
+        mp4: "videos/example.mp4",
+        ogg: "videos/example.ogg",
+        webm: "videos/example.webm"
+    },
+    {
+        thumbnail: "images/gallery/thumbnails/356/9.jpg",
+        mp4: "videos/example.mp4",
+        ogg: "videos/example.ogg",
+        webm: "videos/example.webm"
+    }
+];
+
+
+///
+// Do not edit below this
+///
+jQuery(document).ready(function($) {
+    var Ul = $('.gallery.video .works__row');
+    videos.forEach(function(video, index) {
+        templ = $.htmlTemplate('' +
+            '<a href="#video-'+index+'" data-target="" class="video-link works__item works__item--01" data-toggle="modal" >' +
+                '<div class="works__item_img works__item_img--01">' +
+                    '<img src="#{thumbnail}" alt="">' +
+                '</div>' +
+            '</a>' +
+            '<div id="video-'+index+'" class="modal-container">' +
+                '<div class="list-modal">' +
+                '</div>' +
+            '</div>'
+        );
+
+        Ul.append(templ.evaluate(video));
+    });
+
+    $(document).on('click touchstart', '.gallery.video a.video-link', function(event, ele) {
+        var target = $(event.target).is('.video-link') ? $(event.target) : $(event.target).closest('.video-link');
+        modal = target.attr('href');
+        event.preventDefault();
+        $(modal).addClass('visible');
+    });
+
+    $(document).on('click touchstart', '.gallery.video a.video-link ~ .modal-container', function(event, ele) {
+        event.preventDefault();
+        if(!$(event.target).is('.modal-container')) return;
+        $(event.target).removeClass('visible');
+    });
+});
